11cdec8130cea37ded9fdc1284e3408702917637
adoneza <galupoadonezamae@gmail.com>
2023-01-25 15:36:42 +0800

    addcomment

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -36,6 +36,6 @@ public class MainActivity extends AppCompatActivity {
         rv_users.setLayoutManager(linearLayoutManager);
         rv_users.setAdapter(adapter);
 
-
+        //new branch
     }
 }
